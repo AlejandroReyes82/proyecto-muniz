54d38f86e35c93c6b70e168ae363549c46ccd6d4
Adrian Reyes <areyes@fletes-mexico.com>
2023-12-09 16:08:17 -0600

    Se agrega la funcionalidad de eliminar las materias

--- components/DeleteMateriaButton.jsx
+++ components/DeleteMateriaButton.jsx
@@ -1,19 +1,29 @@
 'use client'
 import React from 'react'
 import { Button } from './ui/button'
+import { deleteMateria } from '@/app/services/materia'
+import { Toaster, toast } from 'sonner'
 
-export default function DeleteUserButton({idmateria, idcarrera}) {
+export default function DeleteUserButton({idmateria, unidad, tema, setMaterias}) {
     
-    const handleRemoveMateria = async ({idmateria, idcarrera}) => {
-        console.log(idmateria, idcarrera)
-        // const response = await fetch(`http://localhost:3000/api/materia/${id}`
+    const handleRemoveMateria = async ({idmateria, unidad, tema}) => {
+        const response = await deleteMateria({idmateria, unidad, tema})
+        if (response) {
+            setMaterias((prev) => prev.filter((item) => item.idmateria !== idmateria))
+            toast.success('Materia eliminada correctamente')
+        } else {
+            toast.error('Error al eliminar materia')
+        }
     }
 
     return (
-        <Button variant="ghost" className="text-red-500" onClick={() => handleRemoveMateria({idmateria, idcarrera})}>
-            <svg arial-label="Eliminar Materia" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-            </svg>
-        </Button>
+        <>
+            <Button variant="ghost" className="text-red-500" onClick={() => handleRemoveMateria({idmateria, unidad, tema})}>
+                <svg arial-label="Eliminar Materia" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                </svg>
+            </Button>
+            <Toaster richColors />
+        </>
     )
 }
