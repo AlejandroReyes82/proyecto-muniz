5a3a3628429150b37439f1f3d3d4fbea031877ab
Adrian Reyes <areyes@fletes-mexico.com>
2023-12-11 18:12:13 -0600

    Se agrega un mensaje de bienvenida en el dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -5,10 +5,13 @@ import { redirect } from 'next/navigation'
 export default function page() {
     const cookieStore = cookies()
     const user = cookieStore.get('usuario')
+    const { nombre, apellido } = JSON.parse(user.value)
     if (!user) {
         redirect('/')
     }
     return (
-        <h1>Dashboard</h1>
+        <div className="flex justify-center items-center h-screen">
+            <h1 className="text-8xl">Bienvenido {nombre} {apellido}</h1>
+        </div>
     )
 }
